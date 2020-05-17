0108e8c039a1b2891453607ba569ffe8a3d1ad58
Ahmed Mohamed Salah <moody.1997@hotmail.com>
2020-05-17 23:16:31 +0200

    Logging local ip addrss and reading main.py

--- index.js
+++ index.js
@@ -2,51 +2,46 @@ var express = require('express')
 var cors = require('cors')
 var fs = require('fs');
 var path = require('path');
-
+var spawn = require("child_process").spawn;
 var multer = require('multer')
 var upload = multer({ dest: __dirname })
+var os = require('os');
 
 const directory = 'uploads';
 
 var app = express()
 
-app.use(cors({credentials: true, origin: true}))
+app.use(cors({ credentials: true, origin: true }))
 
 app.use(function (req, res, next) {
-	const whitelist = ['https://55ahmedsalah.github.io/', 'http://localhost:5500', 'http://127.0.0.1:5500/', 'http://localhost:3000', 'http://127.0.0.1:3000/']; // array of whitelisted website
-	const origin = req.headers["origin"];
-	if(whitelist.includes(origin)){
-		res.header("Access-Control-Allow-Origin", origin)
-		res.header("Access-Control-Allow-Credentials", true);
-		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-	}
-	next();
+  const whitelist = ['https://55ahmedsalah.github.io/', 'http://localhost:5500', 'http://127.0.0.1:5500/', 'http://localhost:3000', 'http://127.0.0.1:3000/']; // array of whitelisted website
+  const origin = req.headers["origin"];
+  if (whitelist.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin)
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  }
+  next();
 });
-
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.set('uploads', path.join(__dirname, 'uploads'))
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 app.post('/upload', upload.single("video"), function (req, res) {
-	console.log("Received file " + req.file.originalname);
-	var src = fs.createReadStream(req.file.path);
-	var dest = fs.createWriteStream('uploads/' + req.file.originalname);
-	src.pipe(dest);
-	src.on('end', function () {
-		fs.unlinkSync(req.file.path);
-		res.json('OK: received ' + req.file.originalname);
-		return;
-	});
-	src.on('error', function (err) { res.json('Upload went wrong!'); });
+  console.log("Received file " + req.file.originalname);
+  var src = fs.createReadStream(req.file.path);
+  var dest = fs.createWriteStream('uploads/' + req.file.originalname);
+  src.pipe(dest);
+  src.on('end', function () {
+    fs.unlinkSync(req.file.path);
+    res.json('OK: received ' + req.file.originalname);
+    return;
+  });
+  src.on('error', function (err) { res.json('Upload went wrong!'); });
 })
 
 app.get('/text', function (req, res) {
   var spawn = require("child_process").spawn;
   var process = spawn('python', ["./main.py"]);
-
   console.log("-------------------------");
   console.log("Sending predicted text...");
 
@@ -91,5 +86,6 @@ app.get('/folder', function (req, res) {
 
 let port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', function () {
-  return console.log("Server started on port " + port + "\n");
+  var ip = os.networkInterfaces()['Wi-Fi'][1].address;
+  return console.log("Server started on " + ip + ":" + port);
 });
